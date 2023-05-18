269793eff79045342658a8819cee3deac254a928
andre-alck <andrealck1@gmail.com>
2023-05-17 21:09:09 -0300

    test: add teste de import; biblioteca googleapis/logging/v2.dart

---
+++ test/testes_de_aprendizagem/import/googleapis_import_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+// ignore: unused_import
+import 'package:googleapis/logging/v2.dart';
+
+void main() {
+  test(
+      'Dada biblioteca googleapis/logging/v2.dart Quando importada Entao teste deve passar',
+      () {});
+}
