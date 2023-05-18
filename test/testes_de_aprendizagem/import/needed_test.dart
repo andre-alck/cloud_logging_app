57610f569a0379458284333d561d30299c28ae60
andre-alck <andrealck1@gmail.com>
2023-05-17 21:32:10 -0300

    test: add teste de import; todas bibliotecas

---
+++ test/testes_de_aprendizagem/import/needed_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:googleapis/logging/v2.dart';
+import 'package:googleapis_auth/auth_io.dart';
+
+void main() {
+  test(
+      'Dado todas bibliotecas necessarias Quando importadas Entao teste deve passar',
+      () {});
+}
