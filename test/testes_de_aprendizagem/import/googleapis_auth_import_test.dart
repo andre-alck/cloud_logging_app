dc7b939b5196ec42f2399d96f1a347e03605a424
andre-alck <andrealck1@gmail.com>
2023-05-17 21:14:36 -0300

    test: add teste de import; biblioteca googleapis_auth/auth_io.dart

---
+++ test/testes_de_aprendizagem/import/googleapis_auth_import_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+// ignore: unused_import
+import 'package:googleapis_auth/auth_io.dart';
+
+void main() {
+  test(
+      'Dada biblioteca googleapis_auth/auth_io.dart Quando importada Entao teste deve passar',
+      () {});
+}
